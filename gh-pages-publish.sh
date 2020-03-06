0bf2b211fe56691d999144bf76137a3b8fa1a203
inversia <mrs.shpritz@gmail.com>
2020-03-06 23:40:12 +0300

    fix

--- gh-pages-publish.sh
+++ gh-pages-publish.sh
@@ -1,5 +1,6 @@
 echo Publishing to GitHub Pages \
 && git checkout gh-pages \
+&& git rm *.html *.js *.css *.map \
 && cp dist/* . \
 && git add --ignore-errors -A \
 && git commit -am "new build" \
