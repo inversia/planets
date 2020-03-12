db1face5f5b7faaec976a8260ba1b080d56a2af2
inversia <mrs.shpritz@gmail.com>
2020-03-12 11:29:20 +0300

    planets Info refactoring

--- src/Info.js
+++ src/Info.js
@@ -5,38 +5,23 @@ export default function Info ({ currentPlanet, setCurrentPlanet }) {
 
     const close = () => setCurrentPlanet (null)
 
+    const planetData = {'Diameter' :        currentPlanet.diameter,
+                        'Rotation period' : currentPlanet.rotation_period,
+                        'Climate' :         currentPlanet.climate,
+                        'Terrain' :         currentPlanet.terrain,
+                        'Surface water' :   currentPlanet.surface_water}
+
     useKey ('Escape', close)
 
     return <div className="info-wrapper">
             <button className="close" onClick={close}></button>
             <div className="list-wrapper">
                 <h1>Planet<span>{currentPlanet.name}</span></h1>
-
                 <ul>
-                <div className="info-item-wrapper">
-                    <label>Diameter:</label>
-                    <li>{currentPlanet.diameter}</li>
-                </div>
-
-                <div className="info-item-wrapper">
-                    <label>Rotation period:</label>
-                    <li>{currentPlanet.rotation_period}</li>
-                </div>
-
-                <div className="info-item-wrapper">
-                    <label>Climate:</label>
-                    <li>{currentPlanet.climate}</li>
-                </div>
-
-                <div className="info-item-wrapper">
-                    <label>Terrain:</label>
-                    <li>{currentPlanet.terrain}</li>
-                </div>
-
-                <div className="info-item-wrapper">
-                    <label>Surface water:</label>
-                    <li>{currentPlanet.surface_water}</li>
-                </div>
+                    {Object.entries (planetData).map (([key, value]) => <div key={key} className='info-item-wrapper'>
+                                                                            <label>{key + ':'}</label>
+                                                                            <li>{value}</li>
+                                                                        </div>)})
                 </ul>
             </div>
         </div>
