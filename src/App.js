7b1c455541a92a057eb621169d54685ebb16f73e
inversia <mrs.shpritz@gmail.com>
2020-03-12 04:53:19 +0300

    restructuring, centering & naming fix

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Info from './Info'
 
 export default function App () {
 
-    const [currentPlanet, setCurrentPlanet] = useState(undefined)
+    const [currentPlanet, setCurrentPlanet] = useState(null)
     const [planets, setPlanets] = useState([])
     const [searchString, setSearchString] = useState ('')
     const [isLoading, setIsLoading] = useState(true)
 
     async function fetchPlanets() {
+        
         try {
-            const { results } = await fetch('https://swapi.co/api/planets/').then (x => x.json())
+            const { results } = await (await fetch('https://swapi.co/api/planets/')).json ()
             setPlanets (results)
             setIsLoading(false)
-        } catch (e) {
-            console.error (e)
+
+        } catch (error) {
+            console.error (error)
             setTimeout (fetchPlanets, 1000)
         }
     }
@@ -23,19 +25,19 @@ export default function App () {
         fetchPlanets()
     }, [])
 
-    function planetFilter (p) {
+    function planetFilter (planet) {
         const str = searchString.toLowerCase()
-        return !str || p.name.toLowerCase().includes (str) || p.terrain.toLowerCase().includes(str)
+        return !str || planet.name.toLowerCase().includes (str) || planet.terrain.toLowerCase().includes(str)
     }
 
     return <div className='container'>
-            <input className='search' value={searchString} onChange={e => setSearchString (e.target.value)} type="text" placeholder="search by planet name, weather or terrain" />
+            <input className='search' value={searchString} onChange={element => setSearchString (element.target.value)} type="text" placeholder="search by planet name, weather or terrain" />
             <div className='table'>
                 <ul>
                     {isLoading && <div className='loading'><CircularLoader /></div>}
                     {planets
                         .filter (planetFilter)
-                        .map(x => <li key={x.name} onClick={() => setCurrentPlanet(x)}>{x.name}</li>)}
+                        .map(planet => <li key={planet.name} onClick={() => setCurrentPlanet(planet)}>{planet.name}</li>)}
                 </ul>
             </div>
             {currentPlanet ?  <Info {...{currentPlanet, setCurrentPlanet}}/> : <h1>Choose planet</h1>}
